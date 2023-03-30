aa4965522a622a62c73c32323b3980c6ff55c04d
xdsolraC <xds0lr4c@gmail.com>
2023-03-29 21:31:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [randomString, setRandomString] = useState("");
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function shuffleString(str) {
+    return str.split("").sort(() => Math.random() - 0.5).join("").toLowerCase();
+  }
+
+  function handleButtonClick() {
+    setRandomString(shuffleString(inputValue));
+  }
+
+  return (
+    <div>
+      <h1>Word letters shuffle app</h1>
+      <div>
+        <input type="text" onChange={handleInputChange} value={inputValue}/>
+        <button onClick={handleButtonClick}>Shuffle</button>
+      </div>
+      <p className="result">{randomString}</p>
+      <footer>by <a href="https://github.com/xdsolraC" target="_blank">xdsolraC</a></footer>
+    </div>
+  );
+}
+
+export default App;
